9016ea3e473976bd368163c99a92cccba434a31c
krecikovsky112 <krecikovsky112@gmail.com>
2021-10-28 12:52:47 +0200

    Add method to count number of upper case letters in string

---
+++ src/main/java/com/example/demo/ControllerStringAnalyzer.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("string_analyzer/")
+public class ControllerStringAnalyzer {
+
+    @GetMapping("upper_case/{text}")
+    public int counterUpperCaseLetters(@PathVariable String text) {
+        int counter = 0;
+        for (char c : text.toCharArray()) {
+            counter += Character.isUpperCase(c) ? 1 : 0;
+        }
+        return counter;
+    }
+}
